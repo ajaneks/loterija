21cc1bf60812449b36bb9b7fcc60bd5907859d25
ajaneks <94831085+ajaneks@users.noreply.github.com>
2021-11-29 11:02:11 +0200

    oterija

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-alert(1)
+const vardi=['Valdis Zatlers','Lauris Reinks', 'Martins Krumins','Abrahams Linkolns', 'Imants Ziedonis']
+const balvas=['3m virve no Ksenukai','Dainu skapis','e-talons','0.5l limpo','datorpele']
+const naudaKopa=1000;
+let uzvaretajuSkaits=3;
+
+Math.random=Math.random()*10;
+
+
+console.log (vardi[random]);
